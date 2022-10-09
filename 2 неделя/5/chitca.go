0f4f4908c7dcde42817f103d5d0e6e9c17afabc0
Serminaz <s99magom@gmail.com>
2022-10-09 17:07:01 +0300

    читка 5

---
+++ 2 неделя/5/chitca.go
@@ -0,0 +1,65 @@
+/*Разработать программу, которая переворачивает подаваемую на ход строку 
+(например: «главрыба — абырвалг»). Символы могут быть unicode.*/
+package main 
+import "fmt"
+func main() { 
+ 
+        input()
+}
+
+func input () {
+
+        var input, word, word2, ff string 
+        var end int
+        for  end != 2 { 
+                fmt.Println("Введи 3  слова через интер")
+                fmt.Scanln(&input)
+                fmt.Scanln(&word2) 
+                fmt.Scanln(&word) 
+                // vers1
+                output  :=   Reverse1(input)
+                fmt.Println("version(1) ",output)
+
+                // vers 2
+                fmt.Println("version(2) ", Reverse2(word2))
+
+                // vers 3
+            
+                 fmt.Println("version(3) ", Reverse3(word))
+
+
+        fmt.Println("желаете продолжить ? (y/n)")
+        fmt.Scanln(&ff) 
+        if ff == "y" {
+                end = 1
+        } else {
+                end = 2
+        }
+}
+}
+
+func Reverse1(s string) string {
+        r := []rune(s)
+        // i,j - начало и конец строки 
+        for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+            r[i], r[j] = r[j], r[i]
+        }
+        return string(r)
+    }
+
+func Reverse2(s string) string {
+        n := len(s)
+        runes := make([]rune, n)
+        for _, rune := range s {
+            n--
+            runes[n] = rune
+        }
+        return string(runes[n:])
+    }
+
+func Reverse3(s string) (result string) {
+        for _,v := range s {
+          result = string(v) + result
+        }
+        return 
+      }
